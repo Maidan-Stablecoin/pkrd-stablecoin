538f99808d5e1583f909ee04ea108893db1229a8
hardsp0rk <107102839+hardsp0rk@users.noreply.github.com>
2023-02-21 11:18:32 +0800

    Add 2-step ownership transfer management

--- src/Stablecoin.sol
+++ src/Stablecoin.sol
@@ -3,11 +3,13 @@
 pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
 
-contract Stablecoin is ERC20PermitUpgradeable {
+contract Stablecoin is ERC20PermitUpgradeable, Ownable2StepUpgradeable {
     function initialize(string memory _name, string memory _symbol) public initializer {
         __Context_init();
         __ERC20_init(_name, _symbol);
         __ERC20Permit_init(_name);
+        __Ownable2Step_init();
     }
 }
